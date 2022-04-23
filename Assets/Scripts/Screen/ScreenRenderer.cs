f9d9ba5829eb7bb44784647937c60339245969dc
Fabian Schober <fschober@imendo.at>
2022-04-23 16:56:39 +0200

    Table Video Testing

--- Assets/Scripts/Screen/ScreenRenderer.cs
+++ Assets/Scripts/Screen/ScreenRenderer.cs
@@ -8,13 +8,15 @@ public class ScreenRenderer : MonoBehaviour
 {
     private Camera _camera;
     private RenderTexture _renderTexture;
-    
-    
+
+    [SerializeField] private Vector2 screenAspect = new Vector2(25f, 32f);
+
+
     // Start is called before the first frame update
     void Start()
     {
         _camera = GetComponent<Camera>();
-        _camera.aspect = 1;
+        _camera.aspect = screenAspect.x / screenAspect.y;
         _renderTexture = _camera.targetTexture;
         //_renderTexture.width = Screen.height;
         //_renderTexture.height = Screen.height;
